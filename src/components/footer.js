358f0c16b060ddee13a95e6e96853ec0b59d4f3c
Pugh Juan <juanpugh98@gmail.com>
2025-11-09 13:45:35 -0300

    Añado footer y header

---
+++ src/components/footer.js
@@ -0,0 +1,45 @@
+export default function Footer() {
+
+    return (
+        <footer>
+
+            <nav>
+
+                <div class="location box">
+                    <img src="img/png/location.png" alt="image location" title="image location" class="image" />
+                    <a href="https://www.google.com/maps/dir//Lanín+2020,+Q8300+Neuquén/@-38.9648456,-68.1290172,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x960a33b795c819a7:0x65c00b69eb2c3f1d!2m2!1d-68.0877317!2d-38.9648529?entry=ttu&g_ep=EgoyMDI0MTAyMy4wIKXMDSoASAFQAw%3D%3D">Donde encontrarnos</a>
+                </div>
+
+                <div class="number box">
+                    <img src="img/png/telephone.png" alt="image telephone" title="image telephone" class="image" />
+                    <a href="tel:+02994478052">0299 447-8052</a>
+                </div>
+
+                <div class="school-page box">
+                    <img src="img/png/logoescuela.png" alt="image escuela" title="image escuela" class="image" />
+                    <a href="https://epet20.edu.ar">Nuestra Escuela</a>
+                </div>
+
+                <div class="facebook-page box">
+                    <img src="img/png/facebook.png" alt="facebook image" title="facebook image" class="image" />
+                    <a href="https://www.facebook.com/EPET20">Facebook</a>
+                </div>
+
+                <div class="instagram-page box">
+                    <img src="img/png/instagram.png" alt="image instagram" title="image instagram" class="image" />
+                    <a href="https://www.instagram.com/epet20educacion/">Instagram</a>
+                </div>
+
+                <div class="twitter-page box">
+                    <img src="img/png/twitter.png" alt="image twitter" title="image twitter" class="image" />
+                    <a href="https://x.com/i/flow/login?redirect_after_login=%2Fepet20educacion">Twitter</a>
+                </div>
+
+
+            </nav>
+
+            <p> &#169;Copyright by LPC Planetario</p>
+
+        </footer>
+    );
+}
---
+++ src/components/header.js
@@ -0,0 +1,47 @@
+export default function Header() {
+
+    return (
+        <header>
+
+            <div class="container-title-logo">
+                <img src="img/logo.png" alt="Logo de empresa" title="Planetario LPC" width="248" height="248" class="logo" />
+                <h1>LPC PLANETARIO</h1>
+            </div>
+
+            <nav>
+
+                <div class="container-card">
+                    <a href="index.html" class="card">
+                        <img src="img/png/home.png" alt="Icono de Inicio" title="¡Ve al inicio!" width="512" height="512" class="front-face" />
+                        <span class="back-face">Inicio</span>
+
+                    </a>
+                </div>
+
+                <div class="container-card">
+                    <a href="solar_system.html" class="card">
+                        <img src="img/png/book.png" alt="Icono de libro" title="¡Aprende sobre el sistema solar!" width="512" height="512" class="front-face" />
+                        <span class="back-face">Aprender</span>
+                    </a>
+                </div>
+
+                <div class="container-card">
+                    <a href="nosotros.html" class="card">
+                        <img src="img/png/team.png" alt="Icono de grupo" title="¡Enterate sobre nosotros!" width="512" height="1000" class="front-face" />
+                        <span class="back-face">Nosotros</span>
+                    </a>
+                </div>
+
+                <div class="container-card">
+                    <a href="login.html" class="card">
+                        <img src="img/png/login.png" alt="image_login" title="¡Inicia sesion en tu cuenta!" width="512" height="512" class="front-face" />
+                        <span class="back-face">Login</span>
+                    </a>
+                </div>
+
+
+            </nav>
+
+        </header>
+    );
+}
